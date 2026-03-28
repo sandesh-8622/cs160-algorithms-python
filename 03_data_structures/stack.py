0a7430cd4aaacb9b7cbcec91262eccb37ab724ee
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-03-28 18:46:08 -0500

    add push and pop methods to the stack

--- 03_data_structures/stack.py
+++ 03_data_structures/stack.py
@@ -5,3 +5,13 @@ Stack - Last in, First Out(LIFO) data structure
 class Stack:
     def __init__(self):
         self.items = []
+    
+    def push(self, item):
+        """Add item to the top of the stack."""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the top item."""
+        if self.is_empty():
+            raise IndexError("pop from empty stack")
+        return self.items.pop()
