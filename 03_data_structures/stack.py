d7e014e861271695b4f671545df26e1886cc8819
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-03-28 20:06:02 -0500

    add working example usage to stack

--- 03_data_structures/stack.py
+++ 03_data_structures/stack.py
@@ -6,7 +6,13 @@ Think of it like plates piled up:
 - You can only look at the top (peek)
 - Last plate in = first plate out (lifo)
 Real world example: Ctrl+z undo button in any software
+
+To understand it very simple manner
+self.items = []
+is the empty pile of plates before anyone puts anything on it
+
 """
+
 
 class Stack:
     def __init__(self):
@@ -35,3 +41,19 @@ class Stack:
     def size(self):
         """Return number of items in the stack."""
         return len(self.items)
+    
+    def __str__(self):
+        return f"Stack (top -> bottom): {self.items[::-1]}"
+if __name__ == "__main__":
+    s = Stack()
+
+    print("Empty?", s.is_empty())  # True, no plates yet
+
+    s.push(10)
+    s.push(20)
+    s.push(30)
+    print(s) # [30,20, 10] --> 30 is on top
+
+    print("Peek:", s.peek())  # 30 --> removed from top
+    print("Pop:", s.pop())
+    print(s)   # [20, 10] --> 30 is gone
