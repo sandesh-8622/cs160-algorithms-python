b88d0e18fbd193458bb966466e03daf187cff1db
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-03-29 22:48:24 -0500

    add Queue class skeleton

---
+++ 03_data_structures/queue.py
@@ -0,0 +1,9 @@
+"""
+Queue - First in, First Out(FIFO) data structure
+Think of it like a McDonald's line:
+-First person in line gets served first
+"""
+
+class Queue:
+    def __init__(self):
+        self.item = []
