23e1c1ac803c4e488694d68e9ad7a62b78ba4395
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-04-04 23:58:33 -0500

    add delete and search methods to LinkedList

--- 03_data_structures/linked_list.py
+++ 03_data_structures/linked_list.py
@@ -10,32 +10,53 @@ Each Node has data and a pointer to the next Node.
 
 class Node:
     def __init__(self, data):
-        self.data = data    # this is the actual value
-        self.next = None # pointer to next node, starts as None
+        self.data = data    # the actual value
+        self.next = None    # pointer to next node, starts as None
+
 class LinkedList:
     def __init__(self):
-        self.head = None   # this is start of the chain, which is empty at first
-    
+        self.head = None    # start of the chain, empty at first
+
     def append(self, data):
-        """ Add a new node to the end of the list."""
-        new_node = Node(data)   # create a new node
-        
-        if self.head is None:    # if the list is empty
-            self.head = new_node   # new node becomes the head
+        """Add a new node to the end of the list."""
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
             return
-        
-        current = self.head   # start at the beginning
-        while current.next:    # until the last node
-            current = current.next  # move to next node
-        current.next = new_node  # attach a new node at the end
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
 
     def prepend(self, data):
-        """ Add a new node to the front of the list."""
+        """Add a new node to the front of the list."""
         new_node = Node(data)
-        new_node.next = self.heads
+        new_node.next = self.head
         self.head = new_node
 
-        
+    def delete(self, data):
+        """Delete the first node with the given data."""
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def search(self, data):
+        """Return True if data is in the list."""
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
     def __str__(self):
         """Print the linked list as a chain."""
         result = []
@@ -45,7 +66,6 @@ class LinkedList:
             current = current.next
         return " -> ".join(result) + " -> None"
 
-# here lets take a example usage to test this
 if __name__ == "__main__":
     ll = LinkedList()
     ll.append(5)
@@ -53,3 +73,7 @@ if __name__ == "__main__":
     ll.append(15)
     ll.prepend(1)
     print(ll)
+    ll.delete(10)
+    print(ll)
+    print("Search 5:", ll.search(5))
+    print("Search 99:", ll.search(99))
