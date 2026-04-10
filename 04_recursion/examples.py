d2a176e5aad9b3359fd1dfbcf19420cb5c696031
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-04-09 23:54:38 -0500

    add fibonacci recursion example

--- 04_recursion/examples.py
+++ 04_recursion/examples.py
@@ -12,5 +12,22 @@ def countdown(n):
     print(n)          # print current number
     countdown(n - 1)  # call yourself with smaller number
 
+def factorial(n):
+        if n == 1:          # base case -> STOP
+            return 1
+        return n * factorial(n - 1)    # recursive case
+
+def fib(n):
+     if n == 0:
+          return 0
+     elif n == 1:
+          return 1
+     else:
+          return fib(n-1) + fib(n-2)
+    
 if __name__ == "__main__":
     countdown(5)
+    print(factorial(5))   
+    print(factorial(3))   
+    print(fib(6))     # 8
+    print(fib(10))     # 55
