377067023af3f25812089e5cf349c5eacf34c3a5
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2026-04-11 20:08:30 -0500

    add binarytree class with insert method

--- 05_trees/binary_tree.py
+++ 05_trees/binary_tree.py
@@ -21,3 +21,28 @@ class TreeNode:
         self.data = data   # the value
         self.left = None   # left child
         self.right = None  # right child
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None   # empty tree, no root yet
+
+    def insert(self, data):
+        """Insert a new node into the tree."""
+        if self.root is None:
+            self.root = TreeNode(data)
+        else:
+            self._insert(self.root, data)
+    
+    def _insert(self, node, data):
+        """Helper function to insert recursively"""
+        if data < node.data:
+            if node.left is None:
+                node.left = TreeNode(data)
+            else:
+                self._insert(node.left, data)
+        
+        else:
+            if node.right is None:
+                node.right = TreeNode(data)
+            else:
+                self._insert(node.right, data)
